6059c36f6052ed3add75e8d70a7e97171f091d40
Tonislav <51849947+tonkata0318@users.noreply.github.com>
2024-03-03 14:11:14 +0200

    Add project files.

---
+++ ToniAuto2003/Data/ToniAutoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToniAuto2003.Data
+{
+    public class ToniAutoDbContext : IdentityDbContext
+    {
+        public ToniAutoDbContext(DbContextOptions<ToniAutoDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ ToniAuto2003/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ToniAuto2003.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
