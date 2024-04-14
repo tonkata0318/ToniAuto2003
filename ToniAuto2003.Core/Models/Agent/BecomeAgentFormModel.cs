9bf4ffdb809d83a31097637f6db8b939384c0051
Tonislav <51849947+tonkata0318@users.noreply.github.com>
2024-04-14 20:07:12 +0300

    Become Agent Model Created

--- ToniAuto2003.Core/Models/Agent/BecomeAgentFormModel.cs
+++ ToniAuto2003.Core/Models/Agent/BecomeAgentFormModel.cs
@@ -1,6 +1,15 @@
-﻿namespace ToniAuto2003.Core.Models.Agent
+﻿using System.ComponentModel.DataAnnotations;
+using static ToniAuto2003.Core.Constants.MessageConstants;
+using static ToniAuto2003.Infrastructure.Constants.DataConstraints;
+
+namespace ToniAuto2003.Core.Models.Agent
 {
     public class BecomeAgentFormModel
     {
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(phonenumberMaxLength,MinimumLength =phoneNumberMinLength , ErrorMessage = LengthMessage)]
+        [Display(Name = "Phone number")]
+        [Phone]
+        public string Phonenumber { get; set; } = null!;
     }
 }
